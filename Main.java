ab5ecc1cd9aecf3e21db3d55e439d571367a3fad
DelfinaBravo <121356466+DelfinaBravo@users.noreply.github.com>
2022-12-24 16:18:40 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+          suma(45, 40, 7);
+
+
+    }
+
+    public static int suma(int a, int b, int c) {
+        int resultado = a + b + c;
+       System.out.println(resultado);
+        return resultado;
+    }
+
+
+
+}
