e4155061525329a3579a34cb5c1c5791c5bfff38
DelfinaBravo <121356466+DelfinaBravo@users.noreply.github.com>
2022-12-31 12:17:02 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-          suma(45, 40, 7);
 
+    Persona persona = new Persona();
+
+    persona.setEdad(17);
+    int edad = persona.getEdad();
+    System.out.println(edad);
+
+    persona.setNombre("Delfina Aylen Bravo");
+    String nombre = persona.getNombre();
+    System.out.println(nombre);
+
+    persona.setNumeroTelefono(3105);
+    int numeroTelefono = persona.getNumeroTelefono();
+    System.out.println(numeroTelefono);
 
     }
+}
+class Persona {
+    private int edad;
+    private String nombre;
+    private int numeroTelefono;
 
-    public static int suma(int a, int b, int c) {
-        int resultado = a + b + c;
-       System.out.println(resultado);
-        return resultado;
+    public void setEdad(int valor){
+        this.edad = valor;
+    }
+    public int getEdad (){
+        return this.edad;
     }
 
+    public void setNombre(String valor){
+        this.nombre = valor;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
 
+    public void setNumeroTelefono(int valor){
+        this.numeroTelefono = valor;
+    }
+    public int getNumeroTelefono(){
+        return this.numeroTelefono;
+    }
 
 }
